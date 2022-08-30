6078632703c07c73377376f5966483e30a1fb304
Tanto van Wijhe <tantovanwijhe@windowslive.com>
2022-08-30 15:32:31 +0200

    Created sample seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   has_many :services, dependent: :destroy
   has_many :user_reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
-  has_many :reviews, through: :bookings 
+  has_many :reviews, through: :bookings
 end
