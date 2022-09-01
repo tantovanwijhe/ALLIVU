16d452da564443c5e18c857ee89a7cc3408917e8
Tanto <105768950+tantovanwijhe@users.noreply.github.com>
2022-09-01 23:30:36 +0200

    Create seeds with images. Rearranged order of Categories.

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,13 +1,25 @@
 class Service < ApplicationRecord
   belongs_to :user
-  has_many :reviews
-  has_many :bookings
+
+  has_many :reviews, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   has_many_attached :photos
 
-  CATEGORIES = ["Food & Drinks", "Healthcare", "Personal Services", "Rentals", "Trips", "Practical Matters"]
+  CATEGORIES = [
+    "Personal Assistant",
+    "Vehicle Rentals",
+    "Tours",
+    "Private Classes",
+    "Beauty",
+    "Sports",
+    "Health",
+    "Souvenirs",
+    "Romantic Services",
+    "Home Entertainment"
+  ]
 
   validates :name, presence: true
   validates :category, inclusion: { in: CATEGORIES }, presence: true
   validates :price, :location, :description, presence: true
-  validates :photos, presence: true
+  # validates :photos, presence: true
 end
