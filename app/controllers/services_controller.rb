347e9021fca951c1162a91d49fd64616bcb7cc5c
Tanto <105768950+tantovanwijhe@users.noreply.github.com>
2022-09-01 14:35:25 +0200

    Tested cloudinary image upload + service#new page

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -13,9 +13,23 @@ class ServicesController < ApplicationController
     @categories = Service::CATEGORIES
   end
 
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+    @service.user = current_user
+    if @service.save
+      redirect_to service_path(@service)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def service_params
-    params.require(:service).permit(:name, :category, :description, :price, :location, :photos [])
+    params.require(:service).permit(:name, :category, :description, :price, :location, photos: [])
   end
 end
