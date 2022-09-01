d304a3411a155896b8b5dc7202f20ce0bf7a42c7
nazanin bos <nazanin.manfaz@gmail.com>
2022-09-01 17:23:34 +0200

    map box

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,12 +1,15 @@
 class Service < ApplicationRecord
+  geocoded_by :location
   belongs_to :user
   has_many :reviews
   has_many :bookings
 
-  CATEGORIES = [ "Food & Drinks", "Healthcare", "Personal Services", "Rentals", "Trips", "Practical Matters"]
+
+  CATEGORIES = ["Food & Drinks", "Healthcare", "Personal Services", "Rentals", "Trips", "Practical Matters"]
 
   validates :name, presence: true
   validates :category, inclusion: { in: CATEGORIES }, presence: true
   validates :price, :location, :description, presence: true
+  after_validation :geocode, if: :will_save_change_to_location?
   # validates :photos, presence: true
 end
