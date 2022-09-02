1effd7330099bf2ba88422c94570783001921b31
Tanto <105768950+tantovanwijhe@users.noreply.github.com>
2022-09-02 16:44:40 +0200

    fix user

--- db/seeds.rb
+++ db/seeds.rb
@@ -106,7 +106,7 @@ snorkel = Service.new(
   description: "In the beginning, we will meet at the meeting point, and get the preparation and we bring the equipment for snorkeling, and after that will jump into the boat and get ready for the Taman jepun beach for the first destination, just about 5 minutes and we arrived at the first destination we will give a little briefing before we jump to the water after we spend about more then 1 hours we will going next to blue lagoon, in here some time we will see a turtle and we can swimming together with the turtle, and after we finish for snorkeling we will heading back to the beach and get the shower and take your lunch, remember in this activity we will bring you free lunch, after that, we will next to visit the hidden gem beach name biastugel beach we Will spend about 1 hours.",
   price: 40,
   location: "Ubud, Bali",
-  user: User.find(10)
+  user: User.all.sample
 )
 
 hike = Service.new(
@@ -116,7 +116,7 @@ hike = Service.new(
   Enjoy breakfast at the peak and capture memories with your camera, while listening to stories of people's lives around Mount Batur told by your local trekking guide.",
   price: 29,
   location: "Kintamani, Bali",
-  user: User.find(11)
+  user: User.all.sample
 )
 
 gate = Service.new(
@@ -125,7 +125,7 @@ gate = Service.new(
   description: "We will visit the gate of heaven at lempuyang temple is the ancient temple is one of bali and most highly regarded temple, on par with mother temple(busakih temple). Photos will be taken by me, using a Fuji camera :).",
   price: 32,
   location: "Ubud, Bali",
-  user: User.find(12)
+  user: User.all.sample
 )
 
 ricefield = Service.new(
@@ -135,7 +135,7 @@ ricefield = Service.new(
   We will cross a private natural jungle and observe the local farmer collecting the grass for balinese cows.",
   price: 15,
   location: "Canggu, Bali",
-  user: User.find(13)
+  user: User.all.sample
 )
 
 ricefield.photos.attach(io: File.open('app/assets/images/seed_img/Rice field walk 1.webp'), filename: 'Rice field walk 1.webp')
@@ -170,7 +170,7 @@ trainer = Service.new(
   description: "My passion is to help people change their lives in the simplest and most realistic way possible. I believe everyone deserves the right to good health and to be happy with themselves inside out. I specialise in mobility and helping you stay strong during your time here!",
   price: 22,
   location: "Kuta, Bali",
-  user: User.find(14)
+  user: User.all.sample
 )
 
 futsal = Service.new(
@@ -179,7 +179,7 @@ futsal = Service.new(
   description: "Come hang out together with me and my friends! I like to meet new people and introduce one the most popular sports here. Join the fun + fanatical competition for both women and men!",
   price: 8,
   location: "Kerobokan, Bali",
-  user: User.find(15)
+  user: User.all.sample
 )
 
 yoga = Service.new(
@@ -188,16 +188,16 @@ yoga = Service.new(
   description: "I am a qualified yoga teacher who can provide sessions in your home. Before becoming a yoga teacher, I worked in the hospitality industry for more than 10 years. Together, we will work on easy mobility exercises to promote good form and posture while also breaking sweat! I can coach all levels from beginners to experienced people. I can also teach children too!",
   price: 25,
   location: "Canggu, Bali",
-  user: User.find(16)
+  user: User.all.sample
 )
 
 surf = Service.new(
   name: "Surfing lessons",
   category: "Sports",
-  description: "Surfboard provided! Beginner lessons usually start with an introduction to basic safety in the water, then we learn standing positions on shore. Once we head into the water I will explain how to get in and out of the ocean safely. Then when we paddle out and find a comfortable position to catch a wave I will help catch the first waves by pushing you in, all you have to do is follow my instructions. I will always give guidance and help you whiles in the water.",
+  description: "Surfboard provided! Beginner lessons usually start with an introduction to basic safety in the water, then we learn standing positions on shore. Once we head into the water I will explain how to get in and out of the ocean safely. Then when we paddle out and all.sampleomfortable position to catch a wave I will help catch the first waves by pushing you in, all you have to do is follow my instructions. I will always give guidance and help you whiles in the water.",
   price: 31,
   location: "Canggu, Bali",
-  user: User.find(17)
+  user: User.all.sample
 )
 
 trainer.photos.attach(io: File.open('app/assets/images/seed_img/Private home trainer 1.jpg'), filename: 'Private home trainer 1.jpg')
@@ -231,7 +231,7 @@ avanza = Service.new(
   description: "Medium size family car, Power steering, Dual SRS Airbag, Air-conditioning, Electric windows, Child safety door lock, Safety belts, Radio, CD-player, AUX & USB port.",
   price: 30,
   location: "Kerobokan, Bali",
-  user: User.find(10)
+  user: User.all.sample
 )
 
 apv = Service.new(
@@ -240,7 +240,7 @@ apv = Service.new(
   description: "Suits for big family or group, Power steering, Dual SRS Airbag, Air-conditioning, Electric windows, Child safety door lock, Safety belts, Radio, CD-player, AUX & USB port.",
   price: 30,
   location: "Denpasar, Bali",
-  user: User.find(10)
+  user: User.all.sample
 )
 
 agya = Service.new(
@@ -249,7 +249,7 @@ agya = Service.new(
   description: "Suits for couple or small family, Power steering, Dual SRS Airbag, Air-conditioning, Electric windows, Child safety door lock, Safety belts, Radio, CD-player, AUX & USB port.",
   price: 30,
   location: "Kuta, Bali",
-  user: User.find(10)
+  user: User.all.sample
 )
 
 scoopy = Service.new(
@@ -258,7 +258,7 @@ scoopy = Service.new(
   description: "Oh look at this pretty thing! The Honda Scoopy is lightweight, automatic and ideal for short distances. To the beach, yoga class or your favourite warung? Pop your phone in the holder on the handlebar and off you go.",
   price: 10,
   location: "Legian, Bali",
-  user: User.find(10)
+  user: User.all.sample
 )
 
 pcx = Service.new(
@@ -267,7 +267,7 @@ pcx = Service.new(
   description: "The new (2022) Honda PCX160 lets you stand out both on the road for comfort and ride quality with its all-new premium and elegant design. Experience it's improved performance whilst enjoying its spacious design.",
   price: 15,
   location: "Petitenget, Bali",
-  user: User.find(10)
+  user: User.all.sample
 )
 
 nmax = Service.new(
@@ -276,7 +276,7 @@ nmax = Service.new(
   description: "Allows you to bring your surfboard along on your day trips. Be aware that the surf racks add an extra foot to the width of you your motorbike and could get caught on to other vehicles whilst passing by or zipping through traffic.",
   price: 20,
   location: "Kerobokan, Bali",
-  user: User.find(10)
+  user: User.all.sample
 )
 
 avanza.photos.attach(io: File.open('app/assets/images/seed_img/avanza1.webp'), filename: 'avanza1.webp')
@@ -310,7 +310,7 @@ massage = Service.new(
   description: "Full body massage. You will receive your passport to bliss. You'll enjoy a five-star divine protocol that includes a silence, sound bath, aromatherapy, foot reflexology massage, and active isolated stretching. I'll make sure you are well traveled and tout our treatment as amongst the best you've ever received. Be ready to be and feel better within an hour.",
   price: 15,
   location: "Canggu, Bali",
-  user: User.find(11)
+  user: User.all.sample
 )
 
 skincare = Service.new(
@@ -319,7 +319,7 @@ skincare = Service.new(
   description: "It will be a warm discussion, engaging class on therapeutic remedies, body care, skin care, hair care such as making body scrub, boreh, hair tonic, face mask, body oil, made out of herbs, leaves and flowers. All skin care ingredients are edible. We will be using ingredients that can be easily found in our kitchen.",
   price: 34,
   location: "Ubud, Bali",
-  user: User.find(13)
+  user: User.all.sample
 )
 
 sound = Service.new(
@@ -328,7 +328,7 @@ sound = Service.new(
   description: "We will start with general introduction, creating intention for the session and calling spirits-healers. After participants lay down on comfortable mattresses and relax bodies through listening to the guided mediation.",
   price: 51,
   location: "Ubud, Bali",
-  user: User.find(12)
+  user: User.all.sample
 )
 
 nailart = Service.new(
@@ -337,7 +337,7 @@ nailart = Service.new(
   description: " I will introduce you to my magic world of nails and design. I will propose you 2 hours of special treat for your hands and nails ,and optionally one hour of teaching you how to do yourself your manicure looking amazing by yourself.",
   price: 36,
   location: "Pererenan, Bali",
-  user: User.find(14)
+  user: User.all.sample
 )
 
 massage.photos.attach(io: File.open('app/assets/images/seed_img/massage1.webp'), filename: 'massage1.webp')
