08d66f587e9ba3c2741daed29ca0bcc878b4265e
Tanto <105768950+tantovanwijhe@users.noreply.github.com>
2022-09-21 12:16:59 +0200

    Skip auhtenitcation for services#show

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,4 +1,5 @@
 class ServicesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
   def show
     @service = Service.find(params[:id])
     @booking = Booking.new
